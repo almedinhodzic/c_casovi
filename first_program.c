67b023c5c2c4c71f90e2fe6881353cfa7ea13e58
almedinhodzic <almedin.json@gmail.com>
2024-06-25 22:04:26 +0200

    Prvi cas

---
+++ first_program.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // Include fajla za input output
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
+
+// printf() za prikazivanje teksta u konzoli
+// 0 ako je sve ok
+// 1 ako ima nekih gresaka
+
+// gcc nam treba za kompajliranje C fajlova
+// gcc -o ako hocemo kompajlirati i pokrenuti C fajl
+// extenzija za C fajlove je .c
+// za vs code pokretanje, skinuti extenziju code runner. Ctrl Alt N
