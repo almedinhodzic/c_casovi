6409141536d862fc614cee3a8ad01f65c0304b35
almedinhodzic <almedin.json@gmail.com>
2024-07-02 23:31:48 +0200

    sad

---
+++ treci cas 03.07.24/zadatak2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  char operator;
+  double num1;
+  double num2;
+  double result;
+
+  // switch na operatoru itd...
+
+  return 0;
+}
+
+
+/*
+  Uzeti inpute za brojeve i operator, i sa switch provjeravati operator
+  da li je +, -, /, * i ako nije validan
+  Izracunati i isprintati rezlutat
+*/
