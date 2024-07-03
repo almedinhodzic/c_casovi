a46438199882626840ae60cb90e9f0f8afb0c606
almedinhodzic <almedin.json@gmail.com>
2024-07-03 22:10:13 +0200

    zadatak update

--- treci cas 03.07.24/zadatak.c
+++ treci cas 03.07.24/zadatak.c
@@ -14,7 +14,7 @@ int main() {
 /*
   Pitati da li zelimo temperaturu u Celsius ili Farenheit
   Sa scanf uzeti input
-  Poslije scanf, koristiti toupper na inputu, jer korisnik moze unijeti i malo slovo c, ali i veliko, pa da nam je laksa provjera, i onda sa if, else if i else rijesiti sljedece
+  Poslije scanf, koristiti toupper (icludiati #include <ctype.h>) na inputu, jer korisnik moze unijeti i malo slovo c, ali i veliko, pa da nam je laksa provjera, i onda sa if, else if i else rijesiti sljedece
   Provjeravati da li je C, ili F
   Napisati u cemu je temperatura
   Onda uzeti input za varijablu temp koja je tipa float
@@ -22,4 +22,9 @@ int main() {
   Formulu za pretvaranje pronaci na netu
   Ako je farenheit, obrnuto odraditi i isprintati. I zaokruziti na 2 broja iza zareza
 
+*/
+
+/*
+  Ako je unos npr 38.5, i izabran celzius, koristiti formuli za izracunavanje celzijusa iz farenheitea, racunati da je 38.5 u farenheitima
+  I obrnuto. 
 */
